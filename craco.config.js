4c47b89b088a788b0a9f2887eb1b386d652ece4b
Alvaro Orellana <alvaroorellana9@hotmail.com>
2023-06-22 01:48:50 +0200

    feat: add path to aliases

--- craco.config.js
+++ craco.config.js
@@ -4,6 +4,7 @@ module.exports = {
     alias: {
       "@Assets": path.resolve(__dirname, "src/assets"),
       "@Components": path.resolve(__dirname, "src/components"),
+      "@Constants": path.resolve(__dirname, "src/constants"),
       "@Hooks": path.resolve(__dirname, "src/hooks"),
       "@Utils": path.resolve(__dirname, "src/utils"),
       "@Views": path.resolve(__dirname, "src/views"),
